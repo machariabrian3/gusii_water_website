5705d2f47c6333912581853cdf0aea3578971e14
machariabrian3 <machariabrian3@gmail.com>
2023-10-14 19:04:29 +0000

    covid and contact us page designs complete

--- app.py
+++ app.py
@@ -13,5 +13,20 @@ def aboutus():
   return render_template("about.html")
 
 
+@app.route("/apiv1/contact")
+def contact():
+  return render_template("contact.html")
+
+
+@app.route("/apiv1/portfolio")
+def portfolio():
+  return render_template("portfolio.html")
+
+
+@app.route("/apiv1/covid19")
+def covid():
+  return render_template("covid.html")
+
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080, debug=True)
