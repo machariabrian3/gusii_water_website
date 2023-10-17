d7493cb842a14d06d3047295271270a86f3bc357
machariabrian3 <machariabrian3@gmail.com>
2023-10-17 02:10:46 +0000

    completed customer application

--- app.py
+++ app.py
@@ -1,112 +1,109 @@
 from flask import Flask, render_template
+from database import load_jobs, load_tenders
 
 app = Flask(__name__)
 
-JOBS_static = [
-  {
+JOBS_static = [{
     "id": 1,
     "title": "Software Engineer",
     "location": "San Francisco",
     "salary": "$100,000 - $120,000"
-  }, {
+}, {
     "id": 2,
     "title": "Data Analyst",
     "location": "New York City",
     "salary": "$80,000 - $90,000"
-  }, {
+}, {
     "id": 3,
     "title": "Marketing Manager",
     "location": "Chicago",
     "salary": "$90,000 - $110,000"
-  }, {
+}, {
     "id": 4,
     "title": "Graphic Designer",
     "location": "Los Angeles",
     "salary": "$70,000 - $80,000"
-  }, {
+}, {
     "id": 5,
     "title": "Project Manager",
     "location": "Seattle",
     "salary": "$95,000 - $105,000"
-  }, {
+}, {
     "id": 6,
     "title": "Sales Representative",
     "location": "Boston",
     "salary": "$60,000 - $70,000"
-  }, {
+}, {
     "id": 7,
     "title": "Product Manager",
     "location": "Austin",
     "salary": "$110,000 - $130,000"
-  }, {
+}, {
     "id": 8,
     "title": "Human Resources Specialist",
     "location": "Washington, D.C.",
     "salary": "$75,000 - $85,000"
-  }, {
+}, {
     "id": 9,
     "title": "Financial Analyst",
     "location": "San Diego",
     "salary": "$85,000 - $95,000"
-  }, {
+}, {
     "id": 10,
     "title": "Customer Service Representative",
     "location": "Miami"
-  }
-]
+}]
 
-tenders_static = [
-  {
+tenders_static = [{
     "id": 1,
     "title": "Software Engineer",
     "location": "San Francisco",
     "salary": "$100,000 - $120,000"
-  }, {
+}, {
     "id": 2,
     "title": "Data Analyst",
     "location": "New York City",
     "salary": "$80,000 - $90,000"
-  }, {
+}, {
     "id": 3,
     "title": "Marketing Manager",
     "location": "Chicago",
     "salary": "$90,000 - $110,000"
-  }, {
+}, {
     "id": 4,
     "title": "Graphic Designer",
     "location": "Los Angeles",
     "salary": "$70,000 - $80,000"
-  }, {
+}, {
     "id": 5,
     "title": "Project Manager",
     "location": "Seattle",
     "salary": "$95,000 - $105,000"
-  }, {
+}, {
     "id": 6,
     "title": "Sales Representative",
     "location": "Boston",
     "salary": "$60,000 - $70,000"
-  }, {
+}, {
     "id": 7,
     "title": "Product Manager",
     "location": "Austin",
     "salary": "$110,000 - $130,000"
-  }, {
+}, {
     "id": 8,
     "title": "Human Resources Specialist",
     "location": "Washington, D.C.",
     "salary": "$75,000 - $85,000"
-  }, {
+}, {
     "id": 9,
     "title": "Financial Analyst",
     "location": "San Diego",
     "salary": "$85,000 - $95,000"
-  }, {
+}, {
     "id": 10,
     "title": "Customer Service Representative",
     "location": "Miami"
-  }
-]
+}]
 
 
 @app.route("/")
@@ -158,13 +155,17 @@ def bowserservices():
 def collapsibleservices():
   return render_template("collapsible-services.html")
 
+
 @app.route("/apiv1/tenders")
 def tenders():
-  return render_template("tenders.html",tenders=tenders_static)
+  tenders = load_tenders()
+  return render_template("tenders.html", tenders=tenders)
+
 
 @app.route("/apiv1/careers")
 def careers():
-  return render_template("careers.html",jobs=JOBS_static)
+  jobs = load_jobs()
+  return render_template("careers.html", jobs=jobs)
 
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STR']
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_jobs():
+  with engine.connect() as conn:
+    result = conn.execute(
+        text('select * from careers where job_status = "ACTIVE"'))
+    jobs = []
+    for row in result.all():
+      jobs.append(row)
+    return jobs
+
+
+def load_tenders():
+  with engine.connect() as conn:
+    result = conn.execute(
+        text('select * from tenders where tender_status ="ACTIVE"'))
+    tenders = []
+    for row in result.all():
+      tenders.append(row)
+    return tenders
